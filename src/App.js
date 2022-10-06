1d7ef5a0ad578b45c9afee3a7391ee76743f5256
Sam Alws <sam@samalws.com>
2022-10-06 02:02:59 -0500

    like button has an effect

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import * as apollo from "@apollo/client"
 import "./App.css"
 import Comment from "./Comment"
 
-const query = apollo.gql`
-  {
-    lookupPostId(tok: "633c968404a49a767f70bf80", id: "633df3e520c69a6ed03f1197") {
+// tok: "633c968404a49a767f70bf80"
+// id: "633df3e520c69a6ed03f1197"
+
+const getPostQuery = apollo.gql`
+query GetPost($tok: String!, $id: String!) {
+  lookupPostId(tok: $tok, id: $id) {
+    id
+    poster {
+      id
+      name
+    }
+    message
+    liked
+    likes
+    replies {
       id
       poster {
         id
@@ -14,28 +26,29 @@ const query = apollo.gql`
       message
       liked
       likes
-      replies {
-        id
-        poster {
-          id
-          name
-        }
-        message
-        liked
-        likes
-      }
     }
   }
-`;
+}
+`
+
+const likePostMutation = apollo.gql`
+mutation LikePost($tok: String!, $id: String!, $like: Boolean!) {
+  setLike(tok: $tok, id: $id, like: $like)
+}
+`
 
 function App() {
-  const { loading, error, data } = apollo.useQuery(query)
+  const tok = "633c968404a49a767f70bf80"
+
+  const { loading, error, data } = apollo.useQuery(getPostQuery, { variables: { tok, id: "633df3e520c69a6ed03f1197" }})
+  const [ likeMut ] = apollo.useMutation(likePostMutation)
+
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
   if (data === null) return <p>Null response :(</p>
 
   const callbacks = {
-    like: (() => alert("like")),
+    like: ((id, like) => likeMut({ variables: { tok, id, like }})), // TODO update the displayed value
     replyBoxType: (() => alert("typed")),
     postReply: (() => alert("replied")),
   }
--- src/LikeButton.js
+++ src/LikeButton.js
@@ -2,7 +2,7 @@ function LikeButton(props) {
   return (<div id="LikeButton">
     <p>{props.info.liked ? "liked" : "not liked"}</p>
     <p>{props.info.likes}</p>
-    <button onClick={props.callback}>Toggle like</button>
+    <button onClick={() => props.callback(props.info.id, !props.info.liked)}>Toggle like</button>
   </div>)
 }
 
