9254c782da715c9163fa0e291146e6301ff9c34a
Sam Alws <sam@samalws.com>
2022-10-07 23:19:54 -0500

    make like button work if state is changed

--- src/LikeButton.js
+++ src/LikeButton.js
@@ -9,15 +9,23 @@ mutation LikePost($tok: String!, $id: String!, $like: Boolean!) {
 }
 `
 
+function infosEqual(a, b) {
+  return a.id === b.id && a.liked === b.liked && a.likes === b.likes
+}
+
 function LikeButton(props) {
-  const [ likeOverride, overrideLike ] = useState(null)
+  const [ likeOverride_, overrideLike ] = useState(null)
+  const [ origInfo, setOrigInfo ] = useState(props.info)
   const [ likePostMut ] = useMutation(likePostMutation)
+
+  const likeOverride = infosEqual(origInfo, props.info) ? likeOverride_ : null
 
   const liked = likeOverride ?? props.info.liked
   const likes = props.info.likes + (likeOverride === true ? 1 : likeOverride === false ? -1 : 0)
 
   function toggleLike() {
     overrideLike(likeOverride === null ? !liked : null)
+    setOrigInfo(props.info)
     likePostMut({ variables: { tok: props.tok, id: props.info.id, like: !liked }})
   }
 
