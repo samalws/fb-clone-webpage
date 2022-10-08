d9d08a1703c897a68992f0bcf366830c5191b341
Sam Alws <sam@samalws.com>
2022-10-08 15:29:12 -0500

    pfp photo upload actually works

--- src/MakeAcct.js
+++ src/MakeAcct.js
@@ -9,8 +9,8 @@ window.Buffer ??= Buffer
 const keccak256 = require("keccak256") // TODO "import keccak256 from keccak256"?
 
 const makeAcctMutation = gql`
-mutation MakeAcct($username: String!, $name: String!, $pfpLink: String!, $pwHashPreSalt: String!) {
-  makeAcct(username: $username, name: $name, pfpLink: $pfpLink, pwHashPreSalt: $pwHashPreSalt) {
+mutation MakeAcct($username: String!, $name: String!, $pfp: Image!, $pwHashPreSalt: String!) {
+  makeAcct(username: $username, name: $name, pfp: $pfp, pwHashPreSalt: $pwHashPreSalt) {
     id
   }
 }
@@ -20,25 +20,14 @@ function Login(props) {
   const [ usernameText, changeUsernameText ] = useState("")
   const [ nameText, changeNameText ] = useState("")
   const [ passwordText, changePasswordText ] = useState("")
-  const [ imgData, setImgData ] = useState(null)
+  const [ image, setImgData ] = useState(null)
   const [ makeAcctMut ] = useMutation(makeAcctMutation)
 
   async function makeAcct() {
-    if (imgData == null) {
+    if (image == null) {
       alert("please upload a profile picture") // TODO
       return
     }
-
-    const pfpResp = await axios.post("https://n5b1h89k3j.execute-api.us-east-1.amazonaws.com/dev/", {
-      image: imgData,
-      mime: "image/png",
-      ext: "png",
-    })
-    if (pfpResp.status !== 200) {
-      alert("failed to upload profile picture") // TODO
-      return
-    }
-    const pfpLink = pfpResp.data
 
     const username = usernameText
     const name = nameText
@@ -48,7 +37,17 @@ function Login(props) {
     changeNameText("")
     changePasswordText("")
 
-    const resp = await makeAcctMut({ variables: { username, name, pfpLink, pwHashPreSalt: keccak256(password).toString("base64") }})
+    const mime = "image/png" // TODO
+    const ext = "png" // TODO
+
+    const pfpResp = await axios.post("https://n5b1h89k3j.execute-api.us-east-1.amazonaws.com/dev/", { image, mime, ext })
+    if (pfpResp.status !== 200) {
+      alert("failed to upload profile picture") // TODO
+      return
+    }
+    const pfp = { ext, ...pfpResp.data }
+
+    const resp = await makeAcctMut({ variables: { username, name, pfp, pwHashPreSalt: keccak256(password).toString("base64") }})
     if (resp.data.makeAcct === null) {
       alert("failed to create account") // TODO
       return
