63f77180409a6ead6629672be653bb5444718cfa
Sam Alws <sam@samalws.com>
2022-10-08 13:30:17 -0500

    image upload almostworks

---
+++ src/ImgUpload.js
@@ -0,0 +1,12 @@
+function ImgUpload(props) {
+  function upload(event) {
+    if (event.target.files.length === 0) return
+    const reader = new FileReader()
+    reader.addEventListener("loadend", () => props.callback(reader.result))
+    reader.readAsBinaryString(event.target.files[0])
+  }
+
+  return <input type="file" onChange={upload} />
+}
+
+export default ImgUpload
--- src/MakeAcct.js
+++ src/MakeAcct.js
@@ -1,9 +1,12 @@
 import { useState } from "react"
 import { gql, useMutation } from "@apollo/client"
+import axios from "axios"
+
+import ImgUpload from "./ImgUpload"
 
 import { Buffer } from "buffer"
 window.Buffer ??= Buffer
-const keccak256 = require("keccak256")
+const keccak256 = require("keccak256") // TODO "import keccak256 from keccak256"?
 
 const makeAcctMutation = gql`
 mutation MakeAcct($username: String!, $name: String!, $pfpLink: String!, $pwHashPreSalt: String!) {
@@ -16,19 +19,33 @@ mutation MakeAcct($username: String!, $name: String!, $pfpLink: String!, $pwHash
 function Login(props) {
   const [ usernameText, changeUsernameText ] = useState("")
   const [ nameText, changeNameText ] = useState("")
-  const [ pfpLinkText, changePfpLinkText ] = useState("")
   const [ passwordText, changePasswordText ] = useState("")
+  const [ imgData, setImgData ] = useState(null)
   const [ makeAcctMut ] = useMutation(makeAcctMutation)
 
   async function makeAcct() {
+    if (imgData == null) {
+      alert("please upload a profile picture") // TODO
+      return
+    }
+
+    const pfpResp = await axios.post("https://n5b1h89k3j.execute-api.us-east-1.amazonaws.com/dev/", {
+      image: imgData,
+      mime: "image/png",
+      ext: "png",
+    })
+    if (pfpResp.status !== 200) {
+      alert("failed to upload profile picture") // TODO
+      return
+    }
+    const pfpLink = pfpResp.data
+
     const username = usernameText
     const name = nameText
-    const pfpLink = pfpLinkText
     const password = passwordText
 
     changeUsernameText("")
     changeNameText("")
-    changePfpLinkText("")
     changePasswordText("")
 
     const resp = await makeAcctMut({ variables: { username, name, pfpLink, pwHashPreSalt: keccak256(password).toString("base64") }})
@@ -40,9 +57,9 @@ function Login(props) {
   }
 
   return (<form action="#" onSubmit={makeAcct}>
+    <ImgUpload callback={setImgData} />
     <input type="text" placeholder="username" value={usernameText} onChange={ (event) => changeUsernameText(event.target.value) } />
     <input type="text" placeholder="name" value={nameText} onChange={ (event) => changeNameText(event.target.value) } />
-    <input type="text" placeholder="pfpLink" value={pfpLinkText} onChange={ (event) => changePfpLinkText(event.target.value) } />
     <input type="password" placeholder="password" value={passwordText} onChange={ (event) => changePasswordText(event.target.value) } />
     <input type="submit" value="Create account" />
   </form>)
